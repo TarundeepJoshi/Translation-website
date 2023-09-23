9876188516b20015898080ce7d0605648fbca559
Tarundeep Joshi <tarundeepjoshi0002@gmail.com>
2023-09-23 18:39:59 +0530

    commit

---
+++ countries.js
@@ -0,0 +1,10 @@
+const countries = {
+    "bn-IN": "Bengali",
+    "en-GB": "English",
+    "gu-IN": "Gujarati",
+    "hi-IN": "Hindi",
+    "pa-IN": "Punjabi",
+    "ta-LK": "Tamil",
+    "te-IN": "Telugu",
+    "mr-IN": "Marathi"
+}
